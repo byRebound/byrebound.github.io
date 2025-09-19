0df16cc9bfdd2e1f7cde3005105fe69a425471c8
Erez Binyamin <>
2025-09-19 16:19:23 -0400

    Added public key to docs/checkout.js

--- docs/checkout.js
+++ docs/checkout.js
@@ -1,6 +1,6 @@
 <script src="https://js.stripe.com/v3/"></script>
 <script>
-  const stripe = Stripe("pk_test_YOUR_PUBLIC_KEY");
+  const stripe = Stripe("pk_live_51PXaDREnSI41RyXT1pZictplLjmivruPeuJn263hAqdMXCl76BxR2lEQ76jfhA4PFcpFscOa7ufBpR3LENRgdcas00IxFELXcP");
 
   document.querySelector("#checkout").addEventListener("click", async () => {
     const response = await fetch("https://your-serverless-function-url.com/api/create-checkout-session", {
